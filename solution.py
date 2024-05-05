c10ba3533403b630710a0d7c55e2663381a7bf79
Rogov-KS <91054242+Rogov-KS@users.noreply.github.com>
2024-05-05 19:45:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as sps
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1022061330 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+
+def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    
+    res_pvalue = sps.ttest_ind(x, y)[1]
+    
+    do_reject = (res_pvalue < alpha)
+    return do_reject # Ваш ответ, True или False
